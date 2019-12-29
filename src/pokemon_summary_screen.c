836af762208bc8ded1d306c2d49a25d60b37d7ce
Sebastian Gabl <sebastian.gabl@student.tugraz.at>
2019-12-29 21:09:06 +0100

    Make TMs infinte use, allow HM replacement

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -2305,7 +2305,7 @@ static void HandleReplaceMoveInput(u8 taskId)
 
 static bool8 CanReplaceMove(void)
 {
-    if (sMonSummaryScreen->firstMoveIndex == MAX_MON_MOVES || sMonSummaryScreen->newMove == MOVE_NONE || IsMoveHm(sMonSummaryScreen->summary.moves[sMonSummaryScreen->firstMoveIndex]) != 1)
+    if (sMonSummaryScreen->firstMoveIndex == MAX_MON_MOVES || sMonSummaryScreen->newMove == MOVE_NONE)
         return TRUE;
     else
         return FALSE;
