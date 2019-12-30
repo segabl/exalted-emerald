576f8126d1cec3e56f9ea76f1a272c6ee093e884
Sebastian Gabl <sebastian.gabl@student.tugraz.at>
2019-12-30 11:05:01 +0100

    Don't remove TMs on use

--- src/party_menu.c
+++ src/party_menu.c
@@ -4702,8 +4702,8 @@ static void Task_LearnedMove(u8 taskId)
     if (move[1] == 0)
     {
         AdjustFriendship(mon, 4);
-        if (item < ITEM_HM01_CUT)
-            RemoveBagItem(item, 1);
+        //if (item < ITEM_HM01_CUT)
+        //    RemoveBagItem(item, 1);
     }
     GetMonNickname(mon, gStringVar1);
     StringCopy(gStringVar2, gMoveNames[move[0]]);
