a7ad73d34a8972a9623d1ffc32390be247694d82
Sebastian Gabl <sebastian.gabl@student.tugraz.at>
2019-12-30 09:30:52 +0100

    Use Gen7 mechanics

--- include/constants/battle_config.h
+++ include/constants/battle_config.h
@@ -43,13 +43,13 @@
 #define GEN_6 3
 #define GEN_7 4
 
-#define B_CRIT_CHANCE               GEN_6 // Chances of a critical hit landing. See atk04_critcalc.
-#define B_FELL_STINGER_STAT_RAISE   GEN_6 // Gen6 Atk+2, Gen7 Atk+3.
-#define B_ABILITY_WEATHER           GEN_6 // Up to gen5 - weather induced by abilities such as Drought or Drizzle lasted till the battle's end or weather change by a move. From Gen6 onwards, weather caused by abilities lasts the same amount of turns as induced from a move.
-#define B_GALE_WINGS                GEN_6 // Gen7 requires full hp.
-#define B_SOUND_SUBSTITUTE          GEN_6 // Starting from gen6 sound moves bypass Substitute.
-#define B_EXP_CATCH                 GEN_6 // Starting from gen6, pokemon get experience from catching.
-#define B_ABILITY_POP_UP            GEN_6 // Starting from gen5, the pokemon abilities are displayed in a pop-up, when they activate in battle.
+#define B_CRIT_CHANCE               GEN_7 // Chances of a critical hit landing. See atk04_critcalc.
+#define B_FELL_STINGER_STAT_RAISE   GEN_7 // Gen6 Atk+2, Gen7 Atk+3.
+#define B_ABILITY_WEATHER           GEN_7 // Up to gen5 - weather induced by abilities such as Drought or Drizzle lasted till the battle's end or weather change by a move. From Gen6 onwards, weather caused by abilities lasts the same amount of turns as induced from a move.
+#define B_GALE_WINGS                GEN_7 // Gen7 requires full hp.
+#define B_SOUND_SUBSTITUTE          GEN_7 // Starting from gen6 sound moves bypass Substitute.
+#define B_EXP_CATCH                 GEN_7 // Starting from gen6, pokemon get experience from catching.
+#define B_ABILITY_POP_UP            GEN_7 // Starting from gen5, the pokemon abilities are displayed in a pop-up, when they activate in battle.
 
 #define B_FAST_INTRO                TRUE // If set to TRUE, battle intro texts print at the same time as animation of a pokemon, as opposing to waiting for the animation to end.
 
