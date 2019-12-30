8425ed957893cd8422c6a5c777e82adede6e845d
Sebastian Gabl <sebastian.gabl@student.tugraz.at>
2019-12-30 17:43:40 +0100

    Remove leftover HM check

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -5716,12 +5716,13 @@ static void Cmd_yesnoboxlearnmove(void)
             else
             {
                 u16 moveId = GetMonData(&gPlayerParty[gBattleStruct->expGetterMonId], MON_DATA_MOVE1 + movePosition);
+                /*
                 if (IsHMMove2(moveId))
                 {
                     PrepareStringBattle(STRINGID_HMMOVESCANTBEFORGOTTEN, gActiveBattler);
                     gBattleScripting.learnMoveState = 6;
                 }
-                else
+                else*/
                 {
                     gBattlescriptCurrInstr = T1_READ_PTR(gBattlescriptCurrInstr + 1);
 
