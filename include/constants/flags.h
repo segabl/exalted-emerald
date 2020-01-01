225ff6d2fdb2f7bd1349329c6e62db6718fbf5d5
Sebastian Gabl <sebastian.gabl@student.tugraz.at>
2020-01-01 16:17:13 +0100

    Remove unused safari expansion flag

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -791,7 +791,9 @@
 #define FLAG_HIDE_ROUTE_109_MR_BRINEY_BOAT                          0x2E8
 #define FLAG_HIDE_LITTLEROOT_TOWN_BRENDANS_HOUSE_BRENDAN            0x2E9
 #define FLAG_HIDE_LITTLEROOT_TOWN_MAYS_HOUSE_MAY                    0x2EA
-#define FLAG_HIDE_SAFARI_ZONE_SOUTH_EAST_EXPANSION                  0x2EB
+
+#define FLAG_UNUSED_0x2EB                                           0x2EB // Unused Flag, was safari zone expansion hide flag
+
 #define FLAG_HIDE_LILYCOVE_HARBOR_EVENT_TICKET_TAKER                0x2EC
 #define FLAG_HIDE_SLATEPORT_CITY_SCOTT                              0x2ED
 #define FLAG_HIDE_ROUTE_101_ZIGZAGOON                               0x2EE
