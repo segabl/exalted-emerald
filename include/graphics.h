88e64a8eeb33df1b5590d81a6b6bf0e6e598f25a
Sebastian Gabl <sebastian.gabl@student.tugraz.at>
2020-01-02 18:32:22 +0100

    Remove double item data

--- include/graphics.h
+++ include/graphics.h
@@ -3776,8 +3776,6 @@ extern const u32 gItemIcon_ThickClub[];
 extern const u32 gItemIconPalette_ThickClub[];
 extern const u32 gItemIcon_Stick[];
 extern const u32 gItemIconPalette_Stick[];
-extern const u32 gItemIcon_DestinyKnot[];
-extern const u32 gItemIconPalette_DestinyKnot[];
 // Contest hold items
 extern const u32 gItemIcon_Scarf[];
 extern const u32 gItemIconPalette_RedScarf[];
