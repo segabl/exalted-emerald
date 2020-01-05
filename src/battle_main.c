80f301376da8eb56844caa864860dd8fb2f0e2e6
Sebastian Gabl <sebastian.gabl@gmx.at>
2020-01-05 10:46:42 +0100

    Nerf paralysis to Gen7 status

--- src/battle_main.c
+++ src/battle_main.c
@@ -4359,7 +4359,7 @@ u32 GetBattlerTotalSpeedStat(u8 battlerId)
 
     // paralysis drop
     if (gBattleMons[battlerId].status1 & STATUS1_PARALYSIS && ability != ABILITY_QUICK_FEET)
-        speed /= 4;
+        speed /= 2;
 
     return speed;
 }
