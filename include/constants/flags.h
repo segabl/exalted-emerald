aa3030286b9ecffdf2594624a73d61fdfe1026d2
Sebastian Gabl <sebastian.gabl@gmx.at>
2020-01-09 21:59:57 +0100

    Add TM86

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1244,9 +1244,9 @@
 #define FLAG_ITEM_SAFARI_ZONE_NORTH_EAST_NUGGET                     0x491
 #define FLAG_ITEM_SAFARI_ZONE_SOUTH_EAST_BIG_PEARL                  0x492
 #define FLAG_ITEM_ROUTE_134_TM80                                    0x493
-#define FLAG_VICTORY_ROAD_B2F_TM81                                  0x494
+#define FLAG_ITEM_VICTORY_ROAD_B2F_TM81                             0x494
+#define FLAG_ITEM_FORTREE_CITY_TM86                                 0x495
 
-#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
 #define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
 #define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
