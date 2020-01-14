49fed589b96bac47f4d007648976b0eae961a3e6
Sebastian Gabl <sebastian.gabl@gmx.at>
2020-01-14 13:56:00 +0100

    Update and add window frames

--- src/text_window.c
+++ src/text_window.c
@@ -27,6 +27,10 @@ static const u8 sTextWindowFrame17_Gfx[] = INCBIN_U8("graphics/text_window/17.4b
 static const u8 sTextWindowFrame18_Gfx[] = INCBIN_U8("graphics/text_window/18.4bpp");
 static const u8 sTextWindowFrame19_Gfx[] = INCBIN_U8("graphics/text_window/19.4bpp");
 static const u8 sTextWindowFrame20_Gfx[] = INCBIN_U8("graphics/text_window/20.4bpp");
+static const u8 sTextWindowFrame21_Gfx[] = INCBIN_U8("graphics/text_window/21.4bpp");
+static const u8 sTextWindowFrame22_Gfx[] = INCBIN_U8("graphics/text_window/22.4bpp");
+static const u8 sTextWindowFrame23_Gfx[] = INCBIN_U8("graphics/text_window/23.4bpp");
+static const u8 sTextWindowFrame24_Gfx[] = INCBIN_U8("graphics/text_window/24.4bpp");
 
 const u16 gTextWindowFrame1_Pal[] = INCBIN_U16("graphics/text_window/1.gbapal");
 static const u16 sTextWindowFrame2_Pal[] = INCBIN_U16("graphics/text_window/2.gbapal");
@@ -48,6 +52,10 @@ static const u16 sTextWindowFrame17_Pal[] = INCBIN_U16("graphics/text_window/17.
 static const u16 sTextWindowFrame18_Pal[] = INCBIN_U16("graphics/text_window/18.gbapal");
 static const u16 sTextWindowFrame19_Pal[] = INCBIN_U16("graphics/text_window/19.gbapal");
 static const u16 sTextWindowFrame20_Pal[] = INCBIN_U16("graphics/text_window/20.gbapal");
+static const u16 sTextWindowFrame21_Pal[] = INCBIN_U16("graphics/text_window/21.gbapal");
+static const u16 sTextWindowFrame22_Pal[] = INCBIN_U16("graphics/text_window/22.gbapal");
+static const u16 sTextWindowFrame23_Pal[] = INCBIN_U16("graphics/text_window/23.gbapal");
+static const u16 sTextWindowFrame24_Pal[] = INCBIN_U16("graphics/text_window/24.gbapal");
 
 static const u16 sUnknown_0851017C[][16] =
 {
@@ -79,7 +87,11 @@ static const struct TilesPal sWindowFrames[WINDOW_FRAMES_COUNT] =
     {sTextWindowFrame17_Gfx, sTextWindowFrame17_Pal},
     {sTextWindowFrame18_Gfx, sTextWindowFrame18_Pal},
     {sTextWindowFrame19_Gfx, sTextWindowFrame19_Pal},
-    {sTextWindowFrame20_Gfx, sTextWindowFrame20_Pal}
+    {sTextWindowFrame20_Gfx, sTextWindowFrame20_Pal},
+    {sTextWindowFrame21_Gfx, sTextWindowFrame21_Pal},
+    {sTextWindowFrame22_Gfx, sTextWindowFrame22_Pal},
+    {sTextWindowFrame23_Gfx, sTextWindowFrame23_Pal},
+    {sTextWindowFrame24_Gfx, sTextWindowFrame24_Pal},
 };
 
 // code
