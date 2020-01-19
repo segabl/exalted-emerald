c45cc0b6a20b2ae5292d9ea9b74ac0926446a6d8
Sebastian Gabl <sebastian.gabl@gmx.at>
2020-01-19 18:11:57 +0100

    Fix facade checking status of target

--- data/battle_ai_scripts.s
+++ data/battle_ai_scripts.s
@@ -2874,7 +2874,7 @@ AI_CV_Sandstorm_End:
 	end
 
 AI_CV_Facade:
-	if_not_status AI_TARGET, STATUS1_POISON | STATUS1_BURN | STATUS1_PARALYSIS | STATUS1_TOXIC_POISON, AI_Ret
+	if_not_status AI_USER, STATUS1_ANY, AI_Ret
 	goto Score_Plus2
 
 AI_CV_FocusPunch:
