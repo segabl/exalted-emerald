7468309f6969d3a4ee6596c3ced877403cfc5bbe
GriffinR <griffin.g.richards@gmail.com>
2020-02-02 13:28:54 -0500

    Use friendship event constants

--- src/party_menu.c
+++ src/party_menu.c
@@ -4790,7 +4790,7 @@ static void Task_LearnedMove(u8 taskId)
 
     if (move[1] == 0)
     {
-        AdjustFriendship(mon, 4);
+        AdjustFriendship(mon, FRIENDSHIP_EVENT_LEARN_TMHM);
     }
     GetMonNickname(mon, gStringVar1);
     StringCopy(gStringVar2, gMoveNames[move[0]]);
