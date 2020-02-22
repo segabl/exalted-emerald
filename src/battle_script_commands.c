c22c0fbc2d6650320e2dcbbc099bea54a4509c36
DizzyEggg <jajkodizzy@wp.pl>
2020-02-22 17:30:07 +0100

    Can't Knock Off mega stones

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -2829,7 +2829,11 @@ void SetMoveEffect(bool32 primary, u32 certain)
                 }
                 break;
             case MOVE_EFFECT_KNOCK_OFF:
-                if (GetBattlerAbility(gEffectBattler) == ABILITY_STICKY_HOLD)
+                if (!CanBattlerGetOrLoseItem(gEffectBattler, gBattleMons[gEffectBattler].item))
+                {
+                    gBattlescriptCurrInstr++;
+                }
+                else if (GetBattlerAbility(gEffectBattler) == ABILITY_STICKY_HOLD)
                 {
                     if (gBattleMons[gEffectBattler].item == 0)
                     {
@@ -2841,9 +2845,8 @@ void SetMoveEffect(bool32 primary, u32 certain)
                         gBattlescriptCurrInstr = BattleScript_StickyHoldActivates;
                         RecordAbilityBattle(gEffectBattler, ABILITY_STICKY_HOLD);
                     }
-                    break;
                 }
-                if (gBattleMons[gEffectBattler].item)
+                else if (gBattleMons[gEffectBattler].item)
                 {
                     side = GetBattlerSide(gEffectBattler);
 
